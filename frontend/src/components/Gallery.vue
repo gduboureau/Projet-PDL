533dadcc9bf0f528e32b3be415a80462741381f2
val-lst <82771795+val-lst@users.noreply.github.com>
2022-03-24 18:42:03 +0100

    correctif gallery + bouton download + ajout gray img

--- frontend/src/components/Gallery.vue
+++ frontend/src/components/Gallery.vue
@@ -17,10 +17,12 @@ api
 
 const isActive = ref("");
 const popupImg = ref("");
-let CurrentId: number;
+const CurrentId = ref(0);
+const CurrentName = ref("");
 
-function ActiveImg(id: number) {
-  CurrentId = id;
+function ActiveImg(id: number, name:string) {
+  CurrentId.value = id;
+  CurrentName.value = name;
   api.getImage(id).then((data: Blob) => {
     const reader = new window.FileReader();
     reader.readAsDataURL(data);
@@ -39,30 +41,38 @@ function Close() {
 }
 
 function Slide(index: number) {
-  if (CurrentId + index < 0) {
-    CurrentId = imageList.value.length - 1;
-    ActiveImg(CurrentId);
-  } else if (CurrentId + index > imageList.value.length - 1) {
-    CurrentId = 0;
-    ActiveImg(CurrentId);
+  if (CurrentId.value + index < imageList.value[0].id) {
+    ActiveImg(imageList.value[imageList.value.length - 1].id, imageList.value[imageList.value.length - 1].name);
+  } else if (CurrentId.value + index > imageList.value[imageList.value.length - 1].id) {
+    ActiveImg(imageList.value[0].id, imageList.value[0].name);
   } else {
-    CurrentId += index;
-    ActiveImg(CurrentId);
+    for (let i=0; i<imageList.value.length ; i++){
+      if (CurrentId.value == imageList.value[i].id){
+        ActiveImg(imageList.value[i+index].id, imageList.value[i+index].name)
+        break;
+      }
+    }
   }
 }
+
 </script>
 
 <template>
   <div class="body">
     <div :class="`popup-img ${isActive}`">
+      <a :href="`/images/${CurrentId}`" :download="CurrentName">
+        <button class="download">
+          <img src="../assets/download.png" alt="Download" />
+        </button>
+      </a> 
       <button class="close" v-on:click="Close()">
-        <img src="../assets/close.png" alt="" />
+        <img src="../assets/close.png" alt="Close" />
       </button>
       <button class="arrow left" v-on:click="Slide(-1)">
-        <img src="../assets/previous.png" alt="" />
+        <img src="../assets/previous.png" alt="ArrowLeft" />
       </button>
       <button class="arrow right" v-on:click="Slide(+1)">
-        <img src="../assets/next.png" alt="" />
+        <img src="../assets/next.png" alt="ArrowRight" />
       </button>
       <div class="large-img">
         <img :src="`${popupImg}`" />
@@ -75,7 +85,7 @@ function Slide(index: number) {
           v-for="image in imageList"
           :id="image.id"
           :key="image.name"
-          v-on:click="ActiveImg(image.id)"
+          v-on:click="ActiveImg(image.id, image.name)"
         />
       </div>
     </div>
@@ -107,8 +117,8 @@ img {
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
-  width: 100.1%;
-  height: 100.1%;
+  width: 100.5%;
+  height: 100.5%;
   background: rgba(0, 0, 0, 0.75);
   display: flex;
   justify-content: center;
@@ -117,52 +127,56 @@ img {
   
 }
 
-.popup-img.active .close,
-.popup-img.active .large-image,
-.popup-img.active .arrow {
-  opacity: 1;
-  transition-delay: 1s;
-}
-
-.close {
+.download, .close, .arrow{
   position:absolute;
-  top: 5vh;
-  right: 3vw;
   cursor: pointer;
   border: none;
   background: none;
+}
+
+.download img, .close img, .arrow img{
+  opacity: 0.7;
+  transition: 0.3s;
+}
+
+.download img:hover, .close img:hover, .arrow img:hover{
+  transform: scale(1.25);
+  opacity: 1;
+  transition: 0.3s;
+}
+
+.download {
+  top: 5vh;
+  right: 6.5vw;
+}
+.download img {
+  width: 1.7vw;
+  height: 1.7vw;
+}
+
+.close {
+  top: 5vh;
+  right: 3vw;
 }
 .close img {
   width: 1.5vw;
   height: 1.5vw;
-  opacity: 70;
-  transition: 0.3s;
-}
-
-.close img:hover {
-  transform: scale(1.25);
-  transition: 0.3s;
 }
 
 .arrow {
-  position: absolute;
   top: 50vh;
   right: 90vw;
-  cursor: pointer;
-  border: none;
-  background: none;
+  
 }
 
 .arrow img {
   width: 2vw;
   height: 2vw;
-  opacity: 70;
-  transition: 0.3s;
+  opacity: 1;
 }
 
 .arrow img:hover {
   transform: scale(1.3);
-  transition: 0.3s;
 }
 
 .right {
